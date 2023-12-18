7ab40f19fb15d55b05ef2ff5664363cac2e00f7b
Dianka <153458862+diiannkaa@users.noreply.github.com>
2023-12-18 23:50:03 +0300

    Добавлена проверка на пустую строку

--- remove_whitespace/main.py
+++ remove_whitespace/main.py
@@ -1,7 +1,11 @@
 import func
-text = input('Введите текст: ')
+while True:
+    text = input('Введите текст: ')
+    if len(text.split()) > 0:
+        clean_text = func.remove_whitespace(text)
+        print(clean_text)
+        break
+    else:
+        print('Введите корректный текст!')
 
-
-clean_text = func.remove_whitespace(text)
-print(clean_text)
 
